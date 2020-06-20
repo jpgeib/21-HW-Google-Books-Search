5f26b00c85adfeed3676591553c4f90a287b55db
James Geib <jpgeib2@gmail.com>
2020-06-19 20:35:30 -0700

    fixed typo in server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.join());
+app.use(express.json());
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
