fc340756d1e3e7ed3373bd55ce2d4546a34e6a73
James Geib <jpgeib2@gmail.com>
2020-06-20 08:32:46 -0700

    build Footer, Form, and Grid components

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <hr />
+            <p className="pull-right">
+                <i className="fab fa-github" />Built using React.js
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
