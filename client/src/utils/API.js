4632018fd2d4e08f4f51125ad24b755b45b3cc32
James Geib <jpgeib2@gmail.com>
2020-06-20 08:53:47 -0700

    set up API.js

---
+++ client/src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+    // Gets books from the Google API
+    getBooks: (q) => {
+      return axios.get("/api/google", { params: { q: "title:" + q } });
+    },
+    // Gets all saved books
+    getSavedBooks: () => {
+      return axios.get("/api/books");
+    },
+    // Deletes the saved book with the given id
+    deleteBook: (id) => {
+      return axios.delete("/api/books/" + id);
+    },
+    // Saves an book to the database
+    saveBook: (bookData) => {
+      return axios.post("/api/books", bookData);
+    }
+  };
