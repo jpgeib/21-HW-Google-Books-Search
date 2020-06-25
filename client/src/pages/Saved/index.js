d9d78c3fd2fbf7b535c38e54842a1e6754627f22
James Geib <jpgeib2@gmail.com>
2020-06-24 22:09:03 -0400

    fixed issue with controllers

--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -8,73 +8,76 @@ import { Col, Row, Container } from "../../components/Grid";
 import { List } from "../../components/List";
 
 class Saved extends Component {
-
     state = {
-        books: []
+      books: []
     };
-
+  
     componentDidMount() {
-        this.getSavedBooks();
-    };
-
-    getSavedBooks = () => {
-        API.getSavedBooks().then(res => this.setState({
-            books: res.data
-        })).catch(err => console.log(err));
-    };
-
-    handleBookDelete = id => {
-        API.deleteBook(id).then(res => this.getSavedBooks());
-    };
-
-    render() {
-        return (
-            <Container>
-                <Row>
-                    <Col size="md-12">
-                        <Jumbotron>
-                            <h1 className="text-center">
-                                <strong>Google Books Search</strong>
-                            </h1>
-                            <h2 className="text-center">Search for and Save Books of Interest.</h2>
-                        </Jumbotron>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col size="md-12">
-                        <Card title="Saved Books" icon="download">
-                            {this.state.books.length ? (
-                                <List>
-                                    {this.state.books.map(book => (
-                                        <Book
-                                            key={book._id}
-                                            title={book.title}
-                                            subtitle={book.subtitle}
-                                            link={book.link}
-                                            authors={book.authors.join(", ")}
-                                            description={book.description}
-                                            image={book.image}
-                                            Button={() => (
-                                                <button
-                                                    onClick={() => this.handleBookDelete(book._id)}
-                                                    className="btn btn-danger ml-2"
-                                                >
-                                                    Delete
-                                                </button>
-                                            )}
-                                        />
-                                    ))}
-                                </List>
-                            ) : (
-                                    <h2 className="text-center">No Saved Books</h2>
-                                )}
-                        </Card>
-                    </Col>
-                </Row>
-                <Footer />
-            </Container>
-        );
+      this.getSavedBooks();
     }
-}
-
-export default Saved;
+  
+    getSavedBooks = () => {
+      API.getSavedBooks()
+        .then(res =>
+          this.setState({
+            books: res.data
+          })
+        )
+        .catch(err => console.log(err));
+    };
+  
+    handleBookDelete = id => {
+      API.deleteBook(id).then(res => this.getSavedBooks());
+    };
+  
+    render() {
+      return (
+        <Container>
+          <Row>
+            <Col size="md-12">
+              <Jumbotron>
+                <h1 className="text-center">
+                  <strong>(React) Google Books Search</strong>
+                </h1>
+                <h2 className="text-center">Search for and Save Books of Interest.</h2>
+              </Jumbotron>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="md-12">
+              <Card title="Saved Books" icon="download">
+                {this.state.books.length ? (
+                  <List>
+                    {this.state.books.map(book => (
+                      <Book
+                        key={book._id}
+                        title={book.title}
+                        subtitle={book.subtitle}
+                        link={book.link}
+                        authors={book.authors.join(", ")}
+                        description={book.description}
+                        image={book.image}
+                        Button={() => (
+                          <button
+                            onClick={() => this.handleBookDelete(book._id)}
+                            className="btn btn-danger ml-2"
+                          >
+                            Delete
+                          </button>
+                        )}
+                      />
+                    ))}
+                  </List>
+                ) : (
+                  <h2 className="text-center">No Saved Books</h2>
+                )}
+              </Card>
+            </Col>
+          </Row>
+          <Footer />
+        </Container>
+      );
+    }
+  }
+  
+  export default Saved;
--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,30 +1,30 @@
 const db = require("../models");
 
 module.exports = {
-    findAll: (req, res) => {
-        db.Book.find(req.query)
-            .then(dbBook => res.json(dbBook))
-            .catch(err => res.status(422).json(err));
-    },
-    findById: (req, res) => {
-        db.Book.findById(req.params.id)
-            .then(dbBook => res.json(dbBook))
-            .catch(err => res.status(422).json(err));
-    },
-    create: (req, res) => {
-        db.Book.create(req.body)
-            .then(dbBook => res.json(dbBook))
-            .catch(err => res.status(422).json(err));
-    },
-    update: (req, res) => {
-        db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
-            .then(dbBook => res.json(dbBook))
-            .catch(err => res.status(422).json(err));
-    },
-    remove: (req, res) => {
-        db.Book.findById(req.params.id)
-            .then(dbBook => dbBook.remove())
-            .then(dbBook => res.json(dbBook))
-            .catch(err => res.status(422).json(err));
-    }
+  findAll: function(req, res) {
+    db.Book.find(req.query)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Book.findById(req.params.id)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Book.create(req.body)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Book.findById(req.params.id)
+      .then(dbBook => dbBook.remove())
+      .then(dbBook => res.json(dbBook))
+      .catch(err => res.status(422).json(err));
+  }
 };
